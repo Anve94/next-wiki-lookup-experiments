b5375e7614ad44b190a2b0ee6d4ffb3c45c8f29c
Stefan Boonstra <boonstra.stefan.wp@gmail.com>
2023-12-17 21:31:12 +0100

    Basic navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import NavBar from "@/app/components/NavBar";
 
 export const metadata = {
   title: 'Create Next App',
@@ -12,7 +13,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-slate-800">
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
